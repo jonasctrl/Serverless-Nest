2567c4bc681da02cdc58dd46ce6b9629250ba8c9
Jonas <jonasggaigalas@gmail.com>
2024-05-13 12:23:34 +0300

    Reverted changes

--- src/infrastructure/modules/health/health.controller.ts
+++ src/infrastructure/modules/health/health.controller.ts
@@ -1,33 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { Readable } from 'stream';
-
-import { S3Service } from 'infrastructure/modules/aws/s3/s3.service';
 
 @ApiTags('Health')
 @Controller('health')
 export class HealthController {
-  constructor(private readonly s3Service: S3Service) {}
-
   @Get('/')
   async check() {
     const time = new Date().toISOString();
-
-    // Create a dummy file and convert it into a readable stream.
-    const fileContent = `Hello, world! - ${time}`;
-    const fileStream = new Readable();
-    fileStream.push(fileContent);
-    fileStream.push(null);
-
-    // Assuming fileKey is unique and using time is one way to ensure that
-    const fileKey = `test-${time}.txt`;
-
-    const url = await this.s3Service.uploadFile('custom-storage-prod', fileKey, fileStream);
-
     return {
       status: 'ok',
       time,
-      url: url,
     };
   }
 }
--- src/infrastructure/modules/health/health.module.ts
+++ src/infrastructure/modules/health/health.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { S3Module } from '../aws/s3/s3.module';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [S3Module],
   controllers: [HealthController],
 })
 export class HealthModule {}
